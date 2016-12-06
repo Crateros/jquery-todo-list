a7074b9c7004c883d8c39161d185b17976a7a3a1
crateros <dsvene@gmail.com>
2016-12-05 23:30:15 -0800

    Add local storage functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
 
+  var listEntry = 0;
+  var newItem = $('input').val();
+
   //Highlight input field on click
   $("input").focus(function(){
     $(this).addClass("outlineClick");
@@ -25,6 +28,10 @@ $( document ).ready(function() {
     //checks for empty submission
     if(newItem !== "") {
       $("ol").append("<li>" + newItem + "</li>");
+      listEntry ++;
+      console.log(listEntry)
+      //Saves item to local storage
+      localStorage.setItem(listEntry, newItem);
     }
   });
 
@@ -35,16 +42,31 @@ $( document ).ready(function() {
         }
     });
 
-  //Upon appending item, clear inpput field
-  $("input")
-
   //Add double click to remove list items
   $(document).on("dblclick", "li", function(){
     $(this).toggleClass("crossOut").fadeOut('slow');
-
+    localStorage.removeItem(listEntry, newItem);
+    listEntry --;
+    console.log(listEntry)
     $("input").focus(function() {
          $(this).val('');
        });
   });
+
+  //Sort list items
   $('ol').sortable();
+
+  //Get local storage working
+  for (i = 1; i <= localStorage.length; i++) {
+  $("ol").append("<li>" + localStorage[i] + "</li>");
+  }
+
+  //Clear list items and local storage on click
+  $("#clearButton").on("click", function(){
+  //   for (var i = 0; i < $("li").length, i ++){
+  //     $("li")[i].remove();
+  //   }
+    localStorage.clear();
+  });
+
 });
